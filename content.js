89c3393949cfafd912be5cab489e8c81339b5af3
Kian Alikhani <ksa33@case.edu>
2018-07-22 18:47:26 -0700

    Ads are now blocked with basic (ugly) overlay

--- content.js
+++ content.js
@@ -6,6 +6,8 @@ window.onload=function(){
 	        var display = target.style.display;
 	        if (display == 'none') {
 	        	console.log('No Ads!');
+	        	var socialBlockContainer = document.getElementById('social-block-container');
+	        	socialBlockContainer.parentNode.removeChild(socialBlockContainer);
 	        } else {
 	        	console.log('Ads!');
 				showSocialMediaContainer();
@@ -22,4 +24,14 @@ window.onload=function(){
 
 function showSocialMediaContainer() {
 	alert('Ads being displayed!');
+
+	if (document.getElementById('social-block-container')) {
+		console.log('social-block-container found!');
+	} else {
+		var target = document.getElementsByClassName('ad-container')[0];
+		document.getElementById('ad-video-player').style.display = 'none';
+		document.getElementsByClassName('ad-player')[0].style.display = 'none';
+		var socialBlockContainer = `<div id="social-block-container" style="background-color: #00FF00; width: ` + target.style.width + `; height: `+ target.style.height + `;">HELLO</div>`;
+		document.getElementsByClassName('ad-container')[0].innerHTML = socialBlockContainer + target.innerHTML;
+	}
 }
